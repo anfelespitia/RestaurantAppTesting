8545aaf7a3d20611439997fb69d76c53841e5fed
anfelespitia <39103110+anfelespitia@users.noreply.github.com>
2018-08-18 20:53:17 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 80 // Change this to your server port
-    return `https://anfelespitia.github.io/restaurant/data/restaurants.json`;
+    return `https://anfelespitia.github.io/RestaurantApp/data/restaurants.json`;
   }
 
   /**
