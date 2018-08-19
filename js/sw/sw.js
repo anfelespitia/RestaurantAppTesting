aee253d7888e17f7a03ddf92edd5ce59e0c1f240
anfelespitia <39103110+anfelespitia@users.noreply.github.com>
2018-08-19 10:33:35 +0200

    Update sw.js

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -55,3 +55,22 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
+
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/styles/main.css',
+       '/scripts/main.min.js',
+       '/sounds/airhorn.mp3'
+     ]);
+   })
+ );
+});
