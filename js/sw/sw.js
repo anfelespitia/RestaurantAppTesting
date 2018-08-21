184f7bdb39ecf63896b92c1e83e3cfe12483d776
anfelespitia <39103110+anfelespitia@users.noreply.github.com>
2018-08-21 18:34:15 +0200

    Update sw.js

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -22,8 +22,30 @@ let urlToCache = [
 ];
 self.addEventListener('install', function (event) {
   event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            console.log(cache);
+        caches.open(staticCacheName)
+      .then(function (cache) {
+          cache.addAll({
+              '/',
+              './restaurant.html',
+              './css/styles.css',
+               './data/restaurants.json',
+               './img/1.jpg',
+                './img/2.jpg',
+                 './img/3.jpg',
+               './img/4.jpg',
+               './img/5.jpg',
+               './img/6.jpg',
+             './img/7.jpg',
+             './img/8.jpg',
+             './img/9.jpg',
+             './img/10.jpg',
+             './js/main.js',
+             './js/restaurant_info.js',
+             './js/dbhelper.js',
+                })
+             })
+      );
+          console.log(cache);
             return cache.addAll(urlToCache);
 
         }).catch(error => {
