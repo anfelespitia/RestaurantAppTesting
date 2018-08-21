a56cab76221f0701aebcf127724131fb5d78294f
anfelespitia <39103110+anfelespitia@users.noreply.github.com>
2018-08-21 19:11:55 +0200

    Update sw.js

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -1,9 +1,7 @@
+var staticCacheName = 'restaurant-cahce-1';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('Restaurant-cache').then(function(cache) {
-      return cache.addAll([
-       '/',
+let urlToCache = [
+    '/',
     './restaurant.html',
     './css/styles.css',
     './data/restaurants.json',
@@ -20,16 +18,40 @@ self.addEventListener('install', function(event) {
     './js/main.js',
     './js/restaurant_info.js',
     './js/dbhelper.js',
-      ]);
-    })
-  );
+
+];
+self.addEventListener('install', function (event) {
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlToCache);
+
+        }).catch(erroe => {
+            console.log(erroe);
+        })
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) return response;
-      return fetch(event.request);
-    })
-  );
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
